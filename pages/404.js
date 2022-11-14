7c6258321cf945b28ff251f2afb20c45fa8738e6
Dheeraj <poojarydheeraj665@gmail.com>
2022-11-14 17:08:32 +0530

    Link component

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,12 @@
 import Link from "next/Link"
+import { useRouter } from "next/router"
 
 const Errorpage = () => {
+    const router  = useRouter();
+    const handleinp =()=> {
+        router.push('/')
+    }
+    
     return (
         <>
             <nav>
@@ -13,6 +19,8 @@ const Errorpage = () => {
                 </ul>
             </nav>
             <h1>Sorry! page not found</h1>
+            <button onClick={handleinp}>back </button>
+            {/* <button onClick={()=>router.push('/')}>back </button> */}
         </>
     )
 }
