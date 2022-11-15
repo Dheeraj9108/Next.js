d36a01ee88efd17c1309cbc7bcb5aedf7e65e8d9
Dheeraj <poojarydheeraj665@gmail.com>
2022-11-15 16:02:54 +0530

    styled-jsx

--- pages/blog/hello.js
+++ pages/blog/hello.js
@@ -5,8 +5,21 @@ const hello = () => {
     // for single class
     // <div className={style.hello}> 
     // for multiple classes
+
     <div className={`${style.hello} ${style.xx}`}> 
-      <h1 style={{color:"green"}}>This is my first next js page</h1>
+      {/* <h1 style={{color:"green"}}>This is my first next js page</h1> */}
+      <h1 >This is my first next js page</h1>
+      <p className='intro'>Intro page</p>
+      <style jsx>
+        {`
+          h1{
+            color:red;
+          }
+          .intro{
+            color:blue
+          }
+        `}
+      </style>
       hello world
     </div>
   )
