21ea7152d0a0a49eb6e4a52f077f76a213822f11
Dheeraj <poojarydheeraj665@gmail.com>
2022-11-15 15:55:02 +0530

    inline css

--- pages/blog/hello.js
+++ pages/blog/hello.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import style from "../../styles/temp.module.css"
 const hello = () => {
   return (
-    <div className='hello'>
+    // for single class
+    // <div className={style.hello}> 
+    // for multiple classes
+    <div className={`${style.hello} ${style.xx}`}> 
+      <h1 style={{color:"green"}}>This is my first next js page</h1>
       hello world
     </div>
   )
