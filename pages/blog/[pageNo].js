5ee7f7881180f7f346dfa83ce312bad3f76a6f7f
Dheeraj <poojarydheeraj665@gmail.com>
2022-11-18 19:57:39 +0530

    getStaticprops

--- pages/blog/[pageNo].js
+++ pages/blog/[pageNo].js
@@ -1,13 +1,26 @@
 import { useRouter } from "next/router"
 
-const pageNo = () => {
+export const getStaticProps = async () => {
+    const res = await fetch("url");
+    const data = await res.json();
+    return {
+        props: {
+            data,
+        },
+    };
+};
+
+const pageNo = ({ data }) => {
     const router = useRouter();
     const pageNumber = router.query.pageNo
-    return (
-        <h1>
-            my blog {pageNumber} content
-        </h1>
-    )
+    data.map((currEle) => {
+        return (
+            <h1>
+                <div> {currEle.id}</div>
+                my blog {pageNumber} content
+            </h1>
+        )
+    })
 }
 
 export default pageNo;
